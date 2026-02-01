cfb7335906cb950196cb9d3cef06661f38170f20
iasdjuliocartagena <jonathanv10022008@gmail.com>
2026-02-01 17:05:31 -0500

    cambio host

--- public/js/config.js
+++ public/js/config.js
@@ -1,1 +1,3 @@
-export const API_URL = "http://localhost:3000/api";
+const API_URL = window.location.hostname === 'localhost' 
+  ? 'http://localhost:3000/api'
+  : 'https://gestion-clubes.onrender.com/api';
